d4a5ed70252041d76c014bc6cb88b25e4f246510
Matan Abudy <matan.abudy@gmail.com>
2025-03-18 15:50:56 +0200

    Revert "move import to main to avoid installation issues"
    This reverts commit 097abdd192184e7e149bd6d9a4ff09fc0dd86936.

--- exh/prop/__init__.py
+++ exh/prop/__init__.py
@@ -1,13 +1,15 @@
+from .formula   import *
+from .predicate import *
+
+true  = Truth()
+false = Falsity()
+
+
+a = Pred(0, name = "a")
+b = Pred(1, name = "b")
+c = Pred(2, name = "c")
+
+
 if __name__ == "__main__":
-    from .formula import *
-    from .predicate import *
-
-    true = Truth()
-    false = Falsity()
-
-    a = Pred(0, name="a")
-    b = Pred(1, name="b")
-    c = Pred(2, name="c")
-
-    f1 = a & b & ~c
-    f2 = a | b & c
+	f1 = a & b & ~c
+	f2 = a | b & c
